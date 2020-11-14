69f11d38f427a727fd15e625b84beafa8e043c5f
Jerezle <iamjeza@gmail.com>
2020-11-14 13:52:05 +0000

    added icon size null protection

--- lib/button_view.dart
+++ lib/button_view.dart
@@ -87,7 +87,7 @@ class SignInButton extends StatelessWidget {
               ? Color.fromRGBO(0, 0, 0, 0.54)
               : Color(0xFFFFFFFF),
           image: Padding(
-            padding: EdgeInsets.symmetric(horizontal: iconSize / 2),
+            padding: EdgeInsets.symmetric(horizontal: (iconSize ?? 36) / 2),
             child: SvgPicture.asset(
               "assets/logos/google_icon.svg",
               height: iconSize ?? 36.0,
@@ -117,7 +117,7 @@ class SignInButton extends StatelessWidget {
           backgroundColor: Color(0xFF1877f2),
           innerPadding: EdgeInsets.zero,
           image: Padding(
-            padding: EdgeInsets.symmetric(horizontal: iconSize / 2),
+            padding: EdgeInsets.symmetric(horizontal: (iconSize ?? 36) / 2),
             child: SvgPicture.asset(
               "assets/logos/f_logo.svg",
               height: iconSize ?? 36.0,
